613f9d5e3d680242fb9511e395e8919e1d875daf
Spencer Williams <s@spencerenglish.com>
2012-02-10 21:51:07 -0800

    something i read about in a place one time

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,6 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# http://ryancannon.com/2008/02/14/eve-on-rails-creating-an-eve-in-game-optimised-version-of-your-rails-site
+Mime::Type.register_alias "text/html", :eve
